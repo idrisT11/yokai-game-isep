729f61a0e8b6fc0bd7e53fe0c2337808b639fb88
Christophe-Saury <95085659+Christophe-Saury@users.noreply.github.com>
2021-11-26 16:58:51 +0100

    classes

--- src/main/java/com/example/yokaigameisep/SideDeck.java
+++ src/main/java/com/example/yokaigameisep/SideDeck.java
@@ -1,10 +1,11 @@
 package com.example.yokaigameisep;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class SideDeck {
     public SideDeck(){
-
+        File pioche = new File();
         ArrayList<IndiceCard> indicePrepared;
 
     }

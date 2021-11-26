442ee7caa57124154f7797ed7d1f9fa5557384e9
idrisT11 <64688223+idrisT11@users.noreply.github.com>
2021-11-26 16:20:48 +0100

    sideDeckys

--- src/main/java/com/example/yokaigameisep/SideDeck.java
+++ src/main/java/com/example/yokaigameisep/SideDeck.java
@@ -1,8 +1,11 @@
 package com.example.yokaigameisep;
 
+import java.util.ArrayList;
+
 public class SideDeck {
     public SideDeck(){
-        
+
+        ArrayList<IndiceCard> indicePrepared;
 
     }
 }

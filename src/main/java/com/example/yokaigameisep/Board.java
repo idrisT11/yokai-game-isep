1d420139bd5721d45a7efedcfa7058d94eede354
Christophe-Saury <95085659+Christophe-Saury@users.noreply.github.com>
2021-12-03 16:11:02 +0100

    Update Board.java

--- src/main/java/com/example/yokaigameisep/Board.java
+++ src/main/java/com/example/yokaigameisep/Board.java
@@ -15,49 +15,48 @@ public class Board {
 
     // Constructor ....................................................................
     public CaseBoard[][] Initialize_Board() {
+
         int min_init_layout = (Constant.LENGTH_MAIN_GRID/2)-1;
         int max_init_layout = (Constant.LENGTH_MAIN_GRID/2)+2;
         int Red_Left=4,Green_Left =4, Blue_Left=4, Purple_Left = 4;
 
         for(int i = min_init_layout; i<max_init_layout; i++){
-            for(int j = min_init_layout; j<max_init_layout; j++){
-                int randnumb0=0;
 
-                mainGrid[i][j].cardOnTop = null;
+            for(int j = min_init_layout; j<max_init_layout; j++){
+
+                int randnumb0=0;
+                mainGrid[i][j].setCardOnTop(null);
 
                 do {randnumb0 = (int) (Math.random() * 4) + 1;
-                }while((randnumb0 == Constant.COLOR_RED && Red_Left==0)||(randnumb0==Constant.COLOR_GREEN &&
-                        Green_Left==0) || (randnumb0 == Constant.COLOR_BLUE && Blue_Left==0) ||
+                }while(
+                        (randnumb0 == Constant.COLOR_RED && Red_Left==0)||
+                        (randnumb0==Constant.COLOR_GREEN && Green_Left==0) ||
+                        (randnumb0 == Constant.COLOR_BLUE && Blue_Left==0) ||
                         (randnumb0 == Constant.COLOR_PURPLE && Purple_Left==0) );
 
                 switch(randnumb0) {
-
                     case COLOR_RED:
-                            mainGrid[i][j].color = COLOR_RED; //set case content to Red
-
+                            mainGrid[i][j].setColor(COLOR_RED); //set case content to Red
                             Red_Left -= 1;
                         break;
 
                     case Constant.COLOR_GREEN:
-                        mainGrid[i][j].color = COLOR_GREEN; //set case value
+                        mainGrid[i][j].setColor(COLOR_GREEN); //set case value
                         Green_Left -= 1;
                         break;
 
                     case Constant.COLOR_PURPLE:
-                        mainGrid[i][j].color = COLOR_RED; //set case value
+                        mainGrid[i][j].setColor(COLOR_RED); //set case value
                         Purple_Left -= 1;
                         break;
 
                     case Constant.COLOR_BLUE:
-                        mainGrid[i][j].color = COLOR_BLUE; //set case value
+                        mainGrid[i][j].setColor(COLOR_BLUE); //set case value
                         Blue_Left -= 1;
                         break;
                     default:
                         // code block
                 }
-
-
-
 
             }
         }

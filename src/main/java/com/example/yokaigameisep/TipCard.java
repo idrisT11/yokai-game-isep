4379896fcdeacfd531671b5d8c3b6fa21a10d697
theotimepoichotte <theotime.poichotte@gmail.com>
2021-12-03 16:03:13 +0100

    initialisation de la pioche

--- src/main/java/com/example/yokaigameisep/TipCard.java
+++ src/main/java/com/example/yokaigameisep/TipCard.java
@@ -3,5 +3,47 @@ package com.example.yokaigameisep;
 public class TipCard {
     private int nbColors;
     private boolean prepared;
+
+    public int getNbColors() {
+        return nbColors;
+    }
+
+    public void setNbColors(int nbColors) {
+        this.nbColors = nbColors;
+    }
+
+    public boolean isPrepared() {
+        return prepared;
+    }
+
+    public void setPrepared(boolean prepared) {
+        this.prepared = prepared;
+    }
+
+    public int getColorValue1() {
+        return colorValue1;
+    }
+
+    public int getColorValue2() {
+        return colorValue2;
+    }
+
+    public void setColorValue2(int colorValue2) {
+        this.colorValue2 = colorValue2;
+    }
+
+    public int getColorValue3() {
+        return colorValue3;
+    }
+
+    public void setColorValue3(int colorValue3) {
+        this.colorValue3 = colorValue3;
+    }
+
     private int colorValue1, colorValue2, colorValue3;
+
+
+    public void setColorValue1(int colorValue1) {
+        this.colorValue1 = colorValue1;
+    }
 }
